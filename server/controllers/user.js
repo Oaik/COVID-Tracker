e2447cbf1fb0b26f4b0c4eeb1d1c42475c359589
Omar Ashraf <omarashrf15@gmail.com>
2022-07-27 07:47:18 +0200

    Added update current user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -11,7 +11,23 @@ const showCurrentUser = (req, res) => {
     return res.json(req.user);
 }
 
+const updateCurrentUser = (req, res) => {
+    const paramId = req.params.id;
+    if(paramId !== req.user.id) {
+        console.log(req.user.id)
+        return res.send("Unauthorized to update the profile");
+    }
+    
+    User.findByIdAndUpdate(req.user.id, {name: req.body.name}).catch((error) => {
+        console.log("Error while updating user");
+
+        return res.json({error: error});
+    });
+
+    return res.json("user updated");
+}
 
 module.exports = {
-    showCurrentUser
+    showCurrentUser,
+    updateCurrentUser
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,10 +1,12 @@
 const express = require("express");
 
-const { showCurrentUser } = require("../controllers/user.js");
+const { showCurrentUser, updateCurrentUser } = require("../controllers/user.js");
 const { validateToken } = require("../middlewares/auth");
 
 const router = express.Router();
 
 router.get("/profile", validateToken, showCurrentUser)
+
+router.put("/profile/:id", validateToken, updateCurrentUser)
 
 module.exports = router;
