b35fe9d4b01f5affbd4fc9b2a5a72542396af14d
Omar Ashraf <omarashrf15@gmail.com>
2022-07-28 07:23:06 +0200

    Add Login page

--- client/src/pages/login/Login.js
+++ client/src/pages/login/Login.js
@@ -1,6 +1,54 @@
+import { useState } from 'react';
+import { useNavigate } from "react-router-dom"
+
+import axios from 'axios';
+
 function Login() {
+    const navigate = useNavigate()
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const changeInputEmail = (event) => {
+        setEmail(event.target.value)
+    }
+
+    const changeInputPassword = (event) => {
+        setPassword(event.target.value)
+    }
+
+    const loginUser = () => {
+        axios.post("http://localhost:8000/auth/login", {
+            email,
+            password
+        }, {            
+            headers: {
+            'Content-Type': 'application/json'
+        }}).then((response) => {
+            if(response.data.accessToken) {
+                navigate('/dashboard');
+            }
+            
+            console.log("LOGIN: ", response.data.accessToken);
+        })
+    }
+
     return (
-        <h1>Hello Login</h1>
+        <div>
+            <div>
+                <span>Email</span>
+                <input name='email' type="text" onChange={changeInputEmail}/>
+            </div>
+
+            <div>
+                <span>password</span>
+                <input name='password' type="text" onChange={changeInputPassword} />
+            </div>
+
+            <button onClick={loginUser} >
+                Login
+            </button>
+        </div>
     )
 }
 
--- client/src/pages/register/Register.js
+++ client/src/pages/register/Register.js
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom"
 
 import axios from 'axios';
 
-
-
 function Register() {
     const navigate = useNavigate()
 
